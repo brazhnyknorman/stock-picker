d3153af9f39f93de28cfd17c19b7b126bb57e329
Norman <164359827+brazhnyknorman@users.noreply.github.com>
2024-03-21 12:45:48 -0400

    Update stock_picker.rb
    Now returns the days to buy and to sell

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,16 +1,21 @@
 def stock_picker(prices)
   i = 0
+  j = 0
+  days = []
   profit = 0
-    j = 0
+  
     while i < prices.length
       while j < prices.length
-        profit = [profit, (prices[j]-prices[i])].max
+        if profit < (prices[j]-prices[i])
+          profit = [profit, (prices[j]-prices[i])].max
+          days = [i,j]
+        end
         j+=1
       end
       i += 1
       j = i
     end
-  return profit
+  return days
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
